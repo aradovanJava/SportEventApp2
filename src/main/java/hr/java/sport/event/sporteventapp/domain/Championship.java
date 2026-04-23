b1766db920a908565214c6c06adcdb9bb0f34ac9
aradovanJava <aleksander.radovan@gmail.com>
2026-04-23 05:09:43 +0200

    Removing Lombok problem

--- src/main/java/hr/java/sport/event/sporteventapp/domain/Championship.java
+++ src/main/java/hr/java/sport/event/sporteventapp/domain/Championship.java
@@ -8,9 +8,9 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class Championship {
     @Id
     private Integer id;
@@ -21,4 +21,37 @@ public class Championship {
             joinColumns = @JoinColumn(name = "CHAMPIONSHIP_ID"),
             inverseJoinColumns = @JoinColumn(name = "CLUB_ID"))
     private List<Club> clubList;
+
+    public Championship() {
+    }
+
+    public Championship(Integer id, String name, List<Club> clubList) {
+        this.id = id;
+        this.name = name;
+        this.clubList = clubList;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Club> getClubList() {
+        return clubList;
+    }
+
+    public void setClubList(List<Club> clubList) {
+        this.clubList = clubList;
+    }
 }
--- src/main/java/hr/java/sport/event/sporteventapp/domain/Club.java
+++ src/main/java/hr/java/sport/event/sporteventapp/domain/Club.java
@@ -6,11 +6,35 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class Club {
     @Id
     private Integer id;
     private String name;
+
+    public Club() {
+    }
+
+    public Club(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
